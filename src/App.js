1296aeb05dd781fa8a6c1678ef5372142ee05aab
Nidhalbenhajsalah <nidal86@gmail.com>
2022-01-22 20:15:04 +0100

    initializing project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Appbar from './components/appbar';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/appbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/appbar.css';
+
+const appbar = () => {
+    return <div className='appbar'>
+        <div className='appbarElements'>
+            <i class="fas fa-bars"></i>
+            <p>SCRUM DASH</p>
+        </div>
+    </div>;
+};
+
+export default appbar;
