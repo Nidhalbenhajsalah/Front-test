dbd87c037f3fa395ed5e43346b7408a0d21cdd8b
Nidhalbenhajsalah <nidal86@gmail.com>
2022-01-25 12:43:14 +0100

    rerieving last version

--- src/components/singleToDo.jsx
+++ src/components/singleToDo.jsx
@@ -1,59 +1,15 @@
-import React from 'react';
-import Modal from 'react-modal'
-// import '../styles/singleToDo.css';
 
-export default function singleToDo({ task, deleteTask, updateTask, modalIsOpen, setModalIsOpen, handleChange, handleUpdate, index }) {
-    return <div className='card' key={index} draggable>
+import React from 'react';
+import '../styles/singleToDo.css';
+
+export default function singleToDo() {
+    return <div className='card'>
         <div className='cardHeader'>
-            <span>{task.title}</span>
+            <span>Title</span>
+            <span>1</span>
         </div>
         <div className='description'>
-            <span>{task.description}</span>
-            <div className='bottom'>
-                <i className="far fa-trash-alt" onClick={() => deleteTask(task._id)}></i>
-                <i className="far fa-edit" onClick={() => setModalIsOpen(true)}></i>
-            </div>
+            <span>description description description description description description</span>
         </div>
-        {/* pop up box for updating task */}
-        <Modal id='modal' isOpen={modalIsOpen}
-            onRequestClose={() => setModalIsOpen(false)}
-            style={
-                {
-                    overlay: {
-                        backgroundColor: 'rgba(0,0,0,0.5)'
-                    },
-                    content: {
-                        color: 'black',
-                        backgroundColor: 'white',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        padding: '20px',
-                        width: '20vw',
-                        height: '10vh',
-                        margin: 'auto',
-                    }
-                }}
-        >
-            <input
-                className='input'
-                name='title'
-                type='text'
-                placeholder='Title'
-                style={{ backgroundColor: 'white', color: 'black' }}
-                defaultValue={task.title}
-                onChange={handleChange}
-
-            />
-            <textarea
-                className='input'
-                name='description'
-                placeholder='Description'
-                style={{ backgroundColor: 'white', color: 'black' }}
-                defaultValue={task.description}
-                onChange={handleChange}
-            />
-
-            <button onClick={() => handleUpdate(task._id)}>update</button>
-        </Modal>
-    </div>
+    </div>;
 }
